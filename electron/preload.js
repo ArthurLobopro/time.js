880db33b4088bf8d22d2f4d6fb446ef5d710c602
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-04-21 20:40:25 -0300

    funções de montagem do header e envio de funções

--- electron/preload.js
+++ electron/preload.js
@@ -1,8 +1,48 @@
 const { ipcRenderer } = require('electron')
+const make_header = require("./make-header.js")
 
 const get = id => document.getElementById(id)
 const formatTime = time => String(time).padStart(2, '0')
 
 window.addEventListener('DOMContentLoaded', ()=> {
-    //Funções a serem adicionadas
+
+    const ismain = true
+
+    const css = document.createElement('link')
+    css.rel = "stylesheet"
+    css.href = ismain ? "header.css" : "../../header.css"
+    document.head.appendChild(css)
+    
+    document.body.style.paddingTop = "30px"
+
+    const header = make_header()
+    document.body.appendChild(header)
+
+    const windowNameLabel = get("window-name")
+    windowNameLabel.innerText = document.querySelector('title').innerText
+
+    const links = document.querySelectorAll("link")
+    for(let e of links){
+        if(e.rel.search("icon") !== -1){
+            const windowIcon = document.getElementById("window-icon")
+            let image = new Image()
+            image.src = e.href
+            image.onload = () => {
+                windowIcon.appendChild(image)
+            }
+        }
+    }
+
+    const minimize_btn = get("minimize")
+    minimize_btn.onclick = ()=> { 
+        ipcRenderer.send('minimize') 
+    }
+
+    const maxime_btn = get("expand")
+    maxime_btn.onclick = () => {
+        ipcRenderer.send('expand')
+    }
+    const close_btn = get("close")
+    close_btn.onclick = () => ipcRenderer.send('close')
+
 })
