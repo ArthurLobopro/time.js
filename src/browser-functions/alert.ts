4a8f24ca67478abd46e70246c1b72a46efaa8b34
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-08-19 10:48:02 -0300

    alert.js moved and converted to ts

---
+++ src/browser-functions/alert.ts
@@ -0,0 +1,36 @@
+import { ipcRenderer } from "electron"
+import path from 'path'
+import injectCSS from "../../electron/cssInject"
+
+
+const target = document.body
+
+interface alertArgs{
+    title: string
+    text: string
+    center?: boolean
+    animation?: boolean
+}
+
+function alert({title, text, center = true, animation = true}: alertArgs) {
+    const div = document.createElement('div')
+    div.className = `full ${center? 'al-center' : ''}`
+    div.id = "alert"
+    div.innerHTML = `
+    <div id="alert-interior" class="${animation ? "down" : ""}">
+        <div class="title">${title}</div>
+        <div class="text"> ${text}</div>
+        <button id="remove-alert">
+            OK
+        </button>
+    </div>`
+    target.appendChild(div)
+    const removeAlert = () => {target.removeChild(div)}
+    (document.getElementById("remove-alert") as HTMLElement).onclick = removeAlert
+}
+
+const appPath = ipcRenderer.sendSync('request-app-path')
+const cssPath = path.resolve(appPath, 'public/css', 'alert.css')
+injectCSS(cssPath)
+
+export default alert
