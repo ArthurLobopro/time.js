5108a9b84d78b14145267419b14bf18ca41c66e0
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-08-19 15:31:53 -0300

    paths updated

--- electron/header/make-header.ts
+++ electron/header/make-header.ts
@@ -1,10 +1,16 @@
+import { ipcRenderer } from 'electron'
+import { resolve } from 'path'
+
+const appPath = ipcRenderer.sendSync('request-app-path')
+
+
 const make_header = () => {
 
-    const path = "assets/windowsIcons/"
+    const iconsPath = resolve(appPath, 'assets', 'windowsIcons')
 
     const header = document.createElement('div')
 
-    header.id= "electron-header"
+    header.id = "electron-header"
     header.innerHTML = `
     <div class="left">
         <div id="window-icon"></div>
@@ -12,13 +18,13 @@ const make_header = () => {
     </div>
     <div class="right">
         <div>
-            <img src="${path}traco.png" alt="Minimaze" title="Minimizar" id="minimize">
+            <img src="${resolve(iconsPath, 'traco.png')}" alt="Minimaze" title="Minimizar" id="minimize">
         </div>
         <div>
-            <img src="${path}expand.png" alt="Maximize" title="Expandir" id="expand">
+            <img src="${resolve(iconsPath, 'expand.png')}" alt="Maximize" title="Expandir" id="expand">
         </div>
         <div>
-            <img src="${path}x.png" alt="Close" title="Fechar" id="close">
+            <img src="${resolve(iconsPath , 'x.png')}" alt="Close" title="Fechar" id="close">
         </div>
     </div>`
     return header
