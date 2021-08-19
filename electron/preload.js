7f039c17952a49951e0def96db6c18a160f7ecf3
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-08-19 10:45:18 -0300

    preload converted to ts and now import App.ts

--- electron/preload.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('DOMContentLoaded', ()=> {
-
-    require("./header/header-actions-renderer.js")
-
-})
---
+++ electron/preload.ts
@@ -0,0 +1,4 @@
+window.addEventListener('DOMContentLoaded', ()=> {    
+    require("./header/header-actions-renderer.js")
+    require('../src/App.js')
+})
